e33ba3c3e7b21090f1166519b8452bdba36ae220
SANDI KOMARA <sandikomara01@gmail.com>
2026-01-02 15:59:38 +0700

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Domain;
+use App\Models\University;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
         $domain = Domain::all();
-        return view('admin.dashboard', compact('domain'));
+
+        return view('admin.dashboard', [
+            'domain' => $domain,
+            'totalUniversity' => University::count(),
+            'totalDomain' => Domain::count(),
+        ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -20,16 +20,16 @@
             <div class="block block-rounded d-flex flex-column h-100 mb-0">
               <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
                 <dl class="mb-0">
-                  <dt class="fs-3 fw-bold">32</dt>
-                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Admin</dd>
+                  <dt class="fs-3 fw-bold">{{ $totalUniversity }}</dt>
+                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Universitas</dd>
                 </dl>
                 <div class="item item-rounded-lg bg-body-light">
                   <i class="far fa-user-circle fs-3 text-primary"></i>
                 </div>
               </div>
               <div class="bg-body-light rounded-bottom">
-                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                  <span>Lihat Semua Admin</span>
+                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="{{ route('admin.ea.university.show') }}">
+                  <span>Lihat Semua Universitas</span>
                   <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
                 </a>
               </div>
@@ -39,54 +39,16 @@
             <div class="block block-rounded d-flex flex-column h-100 mb-0">
               <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
                 <dl class="mb-0">
-                  <dt class="fs-3 fw-bold">124</dt>
-                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Dosen</dd>
+                  <dt class="fs-3 fw-bold">{{ $totalDomain }}</dt>
+                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Komponen / Domain</dd>
                 </dl>
                 <div class="item item-rounded-lg bg-body-light">
                   <i class="far fa-user-circle fs-3 text-primary"></i>
                 </div>
               </div>
               <div class="bg-body-light rounded-bottom">
-                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                  <span>View all customers</span>
-                  <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-xxl-3">
-            <div class="block block-rounded d-flex flex-column h-100 mb-0">
-              <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-                <dl class="mb-0">
-                  <dt class="fs-3 fw-bold">45</dt>
-                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Siswa</dd>
-                </dl>
-                <div class="item item-rounded-lg bg-body-light">
-                  <i class="far fa-paper-plane fs-3 text-primary"></i>
-                </div>
-              </div>
-              <div class="bg-body-light rounded-bottom">
-                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                  <span>View all messages</span>
-                  <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-xxl-3">
-            <div class="block block-rounded d-flex flex-column h-100 mb-0">
-              <div class="block-content block-content-full flex-grow-1 d-flex justify-content-between align-items-center">
-                <dl class="mb-0">
-                  <dt class="fs-3 fw-bold">4.5%</dt>
-                  <dd class="fs-sm fw-medium fs-sm fw-medium text-muted mb-0">Kelas</dd>
-                </dl>
-                <div class="item item-rounded-lg bg-body-light">
-                  <i class="fa fa-chart-bar fs-3 text-primary"></i>
-                </div>
-              </div>
-              <div class="bg-body-light rounded-bottom">
-                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                  <span>View statistics</span>
+                <a class="block-content block-content-full block-content-sm fs-sm fw-medium d-flex align-items-center justify-content-between" href="{{ route('admin.ea.domain.show') }}">
+                  <span>Lihat Komponen / Domain</span>
                   <i class="fa fa-arrow-alt-circle-right ms-1 opacity-25 fs-base"></i>
                 </a>
               </div>
